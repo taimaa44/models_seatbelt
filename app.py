9640f9cc2fe5b043886cf6c15f1f09bc8753b96a
taimaa44 <ta0775176588@gmail.com>
2026-04-16 22:21:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 from fastapi import FastAPI, File, UploadFile
 import tensorflow as tf
 import numpy as np
@@ -12,35 +14,36 @@ MODEL_PATH = "seatbelt_classifier_final.keras"
 
 # تحميل الموديل من Google Drive
 if not os.path.exists(MODEL_PATH):
-    url = "https://drive.google.com/uc?id=14gorD0JYxYif8jiKZt-pYvdzglWyQeqH"
-    gdown.download(url, MODEL_PATH, quiet=False)
+    gdown.download(
+        "https://drive.google.com/uc?id=14gorD0JYxYif8jiKZt-pYvdzglWyQeqH",
+        MODEL_PATH,
+        quiet=False
+    )
 
 # تحميل الموديل
 model = tf.keras.models.load_model(MODEL_PATH)
 
-# prediction
+# أسماء الكلاسات (عدليها حسبك)
+class_names = ["No Seatbelt", "Seatbelt"]
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     with open("input.jpg", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # قراءة الصورة
     img = cv2.imread("input.jpg")
-    img = cv2.resize(img, (224, 224))  # حسب تدريبك
+    img = cv2.resize(img, (300, 300))
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
 
-    # prediction
-    pred = model.predict(img)
-    result = float(pred[0][0])
+    pred = model.predict(img)[0][0]
 
-    # مثال classification
-    if result > 0.5:
-        label = "Seatbelt"
+    if pred > 0.5:
+        label = class_names[1]
     else:
-        label = "No Seatbelt"
+        label = class_names[0]
 
     return {
         "prediction": label,
-        "confidence": result
+        "confidence": float(pred)
     }
