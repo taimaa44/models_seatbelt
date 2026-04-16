80f180f45d5f3112ebee0eab2bada261d1bc26bc
taimaa44 <ta0775176588@gmail.com>
2026-04-16 23:21:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 MODEL_PATH = "seatbelt_classifier_final.keras"
 
-# تحميل الموديل من Google Drive
+# تحميل الموديل
 if not os.path.exists(MODEL_PATH):
     gdown.download(
         "https://drive.google.com/uc?id=14gorD0JYxYif8jiKZt-pYvdzglWyQeqH",
@@ -18,41 +18,26 @@ if not os.path.exists(MODEL_PATH):
         quiet=False
     )
 
-# تحميل الموديل مع حماية
-try:
-    model = tf.keras.models.load_model(MODEL_PATH)
-    print("✅ model loaded successfully")
-except Exception as e:
-    print("❌ model failed:", e)
-    model = None
+model = tf.keras.models.load_model(MODEL_PATH)
+print("✅ model loaded")
 
-# أسماء الكلاسات
 class_names = ["No Seatbelt", "Seatbelt"]
 
-# 🔥 endpoint رئيسي
 @app.get("/")
 def home():
     return {"message": "API is working 🚀"}
 
-# 🔥 prediction
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    
-    # حفظ الصورة
     with open("input.jpg", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # تحقق من الموديل
-    if model is None:
-        return {"error": "Model not loaded"}
-
-    # معالجة الصورة
     img = cv2.imread("input.jpg")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 🔥 مهم
     img = cv2.resize(img, (300, 300))
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
 
-    # prediction
     pred = model.predict(img)[0][0]
 
     label = class_names[1] if pred > 0.5 else class_names[0]
@@ -61,3 +46,10 @@ async def predict(file: UploadFile = File(...)):
         "prediction": label,
         "confidence": float(pred)
     }
+
+# تشغيل السيرفر
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
