f94b9735778166fb34462f3de121dd281c03d7cc
taimaa44 <ta0775176588@gmail.com>
2026-04-17 00:27:14 +0300

    Refactor model loading and prediction process
    Refactored model loading and prediction logic. Removed redundant model loading checks and improved image handling.

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ app = FastAPI()
 
 MODEL_PATH = "seatbelt_classifier_final.keras"
 
-# تحميل الموديل من Google Drive
+# تحميل الموديل
 if not os.path.exists(MODEL_PATH):
-    print("⬇️ Downloading model...")
     gdown.download(
         "https://drive.google.com/uc?id=14gorD0JYxYif8jiKZt-pYvdzglWyQeqH",
         MODEL_PATH,
         quiet=False
     )
 
-model = None
+# تحميل الموديل (بدون compile)
+model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+print("✅ Model loaded")
 
 class_names = ["No Seatbelt", "Seatbelt"]
 
@@ -29,51 +30,22 @@ def home():
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    global model
+    with open("input.jpg", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
 
-    try:
-        # تحميل الموديل أول مرة فقط
-        if model is None:
-            print("⏳ Loading model...")
-            model = tf.keras.models.load_model(MODEL_PATH)
-            print("✅ Model loaded")
+    img = cv2.imread("input.jpg")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (300, 300))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
 
-        # حفظ الصورة
-        file_path = "input.jpg"
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+    pred = model.predict(img)[0][0]
 
-        # قراءة الصورة
-        img = cv2.imread(file_path)
+    label = class_names[1] if pred >= 0.5 else class_names[0]
 
-        if img is None:
-            return {"error": "Image not read correctly"}
+    confidence = float(pred) if pred >= 0.5 else float(1 - pred)
 
-        # معالجة الصورة
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (300, 300))
-        img = img.astype("float32") / 255.0
-        img = np.expand_dims(img, axis=0)
-
-        # prediction
-        pred = model.predict(img, verbose=0)[0][0]
-
-        label = class_names[1] if pred > 0.5 else class_names[0]
-
-        return {
-            "prediction": label,
-            "confidence": float(pred)
-        }
-
-    except Exception as e:
-        print("❌ ERROR:", str(e))
-        return {
-            "error": str(e)
-        }
-
-# تشغيل السيرفر
-import uvicorn
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    return {
+        "prediction": label,
+        "confidence": confidence
+    }
