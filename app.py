cab24240d4c7298635ec2a8806f3d9322788a087
taimaa44 <ta0775176588@gmail.com>
2026-04-16 15:43:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
 from fastapi import FastAPI, File, UploadFile
-from ultralytics import YOLO
-import shutil
+import tensorflow as tf
+import numpy as np
 import cv2
+import shutil
 import os
 import gdown
 
 app = FastAPI()
 
-MODEL_PATH = "best.pt"
+MODEL_PATH = "seatbelt_classifier_final.keras"
 
 # تحميل الموديل من Google Drive
 if not os.path.exists(MODEL_PATH):
     url = "https://drive.google.com/uc?id=14gorD0JYxYif8jiKZt-pYvdzglWyQeqH"
     gdown.download(url, MODEL_PATH, quiet=False)
 
-model = YOLO(MODEL_PATH)
+# تحميل الموديل
+model = tf.keras.models.load_model(MODEL_PATH)
 
+# prediction
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     with open("input.jpg", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    results = model("input.jpg")
+    # قراءة الصورة
+    img = cv2.imread("input.jpg")
+    img = cv2.resize(img, (224, 224))  # حسب تدريبك
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
 
-    output_path = "output.jpg"
-    cv2.imwrite(output_path, results[0].plot())
+    # prediction
+    pred = model.predict(img)
+    result = float(pred[0][0])
 
-    return {"message": "Prediction done"}
+    # مثال classification
+    if result > 0.5:
+        label = "Seatbelt"
+    else:
+        label = "No Seatbelt"
+
+    return {
+        "prediction": label,
+        "confidence": result
+    }
